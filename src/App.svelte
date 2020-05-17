070d7b22b88b215c48defba082cc144788b9a8a0
Julien Deflaux <jdeflaux@gmail.com>
2020-05-17 08:09:02 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Number from "./Number.svelte";
+  export let numbers;
+  export let max = 100;
+  let number;
+
+  let unit;
+  let tens;
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  function onClick() {
+    const n = getRandomInt(max);
+    number = numbers[n % numbers.length];
+  }
+  onClick();
+</script>
+
+<main class="container">
+  <h1 class="mt-5">Learn Korean Numbers !</h1>
+
+  <form class="form-inline mb-3 mt-3">
+    <label for="max" class="mr-2">max</label>
+    <input type="text" class="form-control mr-2" id="max" placeholder="max" bind:value={max}/>
+    <button class="btn btn-primary" on:click={onClick}>Next Number</button>
+  </form>
+  
+  <Number {number} />
+</main>
---
+++ src/Number.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let number;
+</script>
+
+<style>
+  .card .reveal {
+    visibility: hidden;
+  }
+
+  .card:hover .reveal {
+    visibility: visible;
+  }
+</style>
+
+{#if number && number.id !== undefined}
+<div class="card">
+  <div class="card-body text-center">
+    <h1>{number.id}</h1>
+
+    <div class="row">
+      <span class="col-sm reveal text-right">
+          <h2 >{number.sino}</h2>
+          <span class="badge badge-info">sino</span>
+      </span>
+
+      <span class="col-sm reveal text-left">
+          <h2 >{number.native}</h2>
+          <span class="badge badge-info">native</span>
+      </span>
+    </div>
+  </div>
+</div>
+{/if}
